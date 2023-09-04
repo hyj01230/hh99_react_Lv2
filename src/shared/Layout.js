90ed79ee454166fc0f90d1f32532eba595e32c9c
hyj01230 <137272433+hyj01230@users.noreply.github.com>
2023-09-05 00:46:47 +0900

    Header 태그 차이점 추가

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -28,13 +28,28 @@ function Header() {
 // 모든 페이지 레이아웃의 제일 큰 틀!!
 // 모든 페이지는 Wrap, Header를 가진다!
 // Router.js 에서 import해서 받아주면, 모든 페이지에서 적용이 된다!!
+// Header와 children이 별도로 렌더링!
 function Layout({ children }) {
   return (
     <Wrap>
       <Header />
-        {children}
+      {children}
     </Wrap>
   )
 }
 
 export default Layout;
+
+
+// 이렇게 작성하면 Header 안에 children을 렌더링해서 상단만 출력됨
+// function Layout({ children }) {
+//   return (
+//     <Wrap>
+//       <Header>
+//         {children}
+//       </Header>
+//     </Wrap>
+//   )
+// }
+
+// export default Layout;
