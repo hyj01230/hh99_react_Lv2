b9fe0e8aae30d56df6ebdc8ef905e2cbfebca45f
pmaker126 <pmaker126@gmail.com>
2023-09-05 00:22:57 +0900

    고정 헤더 완성!

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
 const Wrap = styled.div`
-text-align: center;
+  text-align: center;
   min-width: 800px;
   max-width: 1200px;
   margin: 0 auto;
@@ -28,11 +28,11 @@ function Header() {
 // 모든 페이지 레이아웃의 제일 큰 틀!!
 // 모든 페이지는 Wrap, Header를 가진다!
 // Router.js 에서 import해서 받아주면, 모든 페이지에서 적용이 된다!!
-function Layout() {
+function Layout({ children }) {
   return (
     <Wrap>
-      <Header>
-      </Header>
+      <Header />
+        {children}
     </Wrap>
   )
 }
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -6,14 +6,14 @@ import Layout from "./Layout";
 const Router = () => {
   return (
     <BrowserRouter>
-      {/* <Layout> */}
+      <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="detail" element={<Detail />} /> */}
           {/* 삭제해도 문제없음! 밑에 주소로 연결되어있기 떄문에!!*/}
           <Route path="detail/:id" element={<Detail />} />
         </Routes>
-      {/* </Layout> */}
+      </Layout>
     </BrowserRouter>
   );
 };
